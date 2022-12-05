32f6d3bbe15548da8eb054b5b5335b6074ec22e1
Nikita Boev <nekitcudder@yandex.ru>
2022-12-05 13:07:19 +0300

    hotfix: исправить jwt

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -10,7 +10,7 @@ module.exports.auth = (req, res, next) => {
   }
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new UnauthorizedError('Ошибка авторизации #2.'));
   }
